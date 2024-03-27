12666f88f50ca6665a7a4c6ea22f294a9aa11570
Anastasia Proskuryakova <anproskuryakova@ozon.ru>
2024-03-27 19:03:58 +0500

    sorting tests done

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -5,85 +5,50 @@ import { RadioInput } from '../ui/radio-input/radio-input';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import styles from './sorting-page.module.css';
 import { ElementStates } from '../../types/element-states';
-import { DELAY_IN_MS } from '../../constants/delays';
+import { SHORT_DELAY_IN_MS } from '../../constants/delays';
 import { sleep } from '../../utils/utils-functions';
+import { bubbleSortSteps, selectionSortSteps } from './utils';
 
-export const SortingPage= () => {
-  const [selectedSortingMethod, setSelectedSortingMethod] = useState('selection');
-  const [currArr, setCurrArr] = useState<number[]>([]);
+export const SortingPage = () => {
+  const [selectedSortingMethod, setSelectedSortingMethod] = useState<'selection' | 'bubble'>('selection');
+  const [currArr, setCurrArr] = useState<{ value: number; state: ElementStates }[]>([]);
   const [isSorting, setIsSorting] = useState(false);
-  const [comparingIndexes, setComparingIndexes] = useState<number[]>([]);
-  const [sortedIndexes, setSortedIndexes] = useState<number[]>([]);
 
   useEffect(() => {
     generateRandomArray();
   }, []);
 
   const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedSortingMethod(event.target.value);
+    setSelectedSortingMethod(event.target.value as 'selection' | 'bubble');
   };
 
   const generateRandomArray = () => {
-    const length = Math.floor(Math.random() * (17 - 3 + 1)) + 3;
-    const arr = Array.from({ length }, () => Math.floor(Math.random() * 101));
-    setCurrArr(arr);
-    setComparingIndexes([]);
-    setSortedIndexes([]);
+    const length = Math.floor(Math.random() * (15 - 5 + 1)) + 5; 
+    const arr = Array.from({ length }, () => ({
+      value: Math.floor(Math.random() * 100),
+      state: ElementStates.Default,
+    }));
+    setCurrArr([...arr]);
   };
 
-  const selectionSort = async(newArr:number[],direction:'ascending'|'descending', len:number)=>{
-    for (let i = 0; i < len; i++) {
-      let minOrMaxIndex = i;
-      for (let j = i + 1; j < len; j++) {
-        setComparingIndexes([minOrMaxIndex, j]);
-        await sleep(DELAY_IN_MS);
-        const shouldSwap = direction === 'ascending' ? newArr[j] < newArr[minOrMaxIndex] : newArr[j] > newArr[minOrMaxIndex];
-        if (shouldSwap) {
-          minOrMaxIndex = j;
-        }
-      }
-      if (minOrMaxIndex !== i) {
-        [newArr[i], newArr[minOrMaxIndex]] = [newArr[minOrMaxIndex], newArr[i]];
-        await sleep(DELAY_IN_MS);
-      }
-      setSortedIndexes((prev) => [...prev, i]);
-      setCurrArr([...newArr]);
+  const visualizeSteps = async (steps: { value: number; state: ElementStates }[][]) => {
+    for (let step of steps) {
+      setCurrArr(step);
+      await sleep(SHORT_DELAY_IN_MS);
     }
-  }
-
-  const bubbleSort = async(newArr:number[], direction:'ascending'|'descending', len:number) =>{
-    for (let i = 0; i < len; i++) {
-      for (let j = 0; j < len - i - 1; j++) {
-        setComparingIndexes([j, j + 1]);
-        await sleep(DELAY_IN_MS);
-        const shouldSwap = direction === 'ascending' ? newArr[j] > newArr[j + 1] : newArr[j] < newArr[j + 1];
-        if (shouldSwap) {
-          let temp = newArr[j];
-          newArr[j] = newArr[j + 1];
-          newArr[j + 1] = temp;
-
-          await sleep(DELAY_IN_MS);
-          setCurrArr([...newArr]);
-        }
-      }
-      setSortedIndexes(prev => [...prev, len - i - 1]);
-    }
-  }
+  };
 
   const sortArray = async (direction: 'ascending' | 'descending') => {
     setIsSorting(true);
-    setSortedIndexes([])
-    const newArr = [...currArr];
-    const len = newArr.length;
-
+    let steps = [] as { value: number; state: ElementStates}[][];
+    currArr.forEach(item => item.state = ElementStates.Default); 
     if (selectedSortingMethod === 'selection') {
-      selectionSort(newArr, direction, len)
-    } 
-    if (selectedSortingMethod === 'bubble') {
-      bubbleSort(newArr,direction,len)
+      steps = selectionSortSteps(currArr, direction);
+    } else if (selectedSortingMethod === 'bubble') {
+      steps = bubbleSortSteps(currArr, direction);
     }
+    await visualizeSteps(steps);
     setIsSorting(false);
-    setComparingIndexes([]);
   };
 
   const startSorting = (direction: 'ascending' | 'descending') => {
@@ -100,15 +65,13 @@ export const SortingPage= () => {
         <div className={styles.buttonList}>
           <Button text={'По возрастанию'} onClick={() => startSorting('ascending')} disabled={isSorting} />
           <Button text={'По убыванию'} onClick={() => startSorting('descending')} disabled={isSorting} />
-          <Button text={'Новый массив'} onClick={generateRandomArray} disabled={isSorting}/>
+          <Button text={'Новый массив'} onClick={generateRandomArray} disabled={isSorting} />
         </div>
       </div>
       <div className={styles.arrayDisplay}>
-        {currArr.map((value, index) => (
-          <Column key={index} index={value} state={sortedIndexes.includes(index) ? ElementStates.Modified : comparingIndexes.includes(index) ? ElementStates.Changing : ElementStates.Default} />
-        ))}
+        {currArr.map((item, index) => (
+         <Column key={index} index={item.value} state={item.state} />))}
       </div>
     </SolutionLayout>
   );
 };
-
